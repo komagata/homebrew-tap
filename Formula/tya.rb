55015d3a9b575b8601106abc74844d01d96f0226
Masaki Komagata <komagata@gmail.com>
2026-05-08 08:01:46 +0900

    Add tya v0.2.0 formula

---
+++ Formula/tya.rb
@@ -0,0 +1,23 @@
+class Tya < Formula
+  desc "Small indentation-based dynamic language"
+  homepage "https://tya-lang.org/"
+  url "https://github.com/komagata/tya/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "1b8f0a0adfa7b03107eda3ed1f9539a5cdb7547d2eab5b57772f3019d32593bd"
+  head "https://github.com/komagata/tya.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(output: bin/"tya"), "./cmd/tya"
+    (share/"tya/runtime").install Dir["runtime/*"]
+  end
+
+  test do
+    (testpath/"hello.tya").write <<~TYA
+      print "Hello, Tya"
+    TYA
+
+    assert_equal "0.2.0\n", shell_output("#{bin}/tya version")
+    assert_equal "Hello, Tya\n", shell_output("#{bin}/tya run #{testpath}/hello.tya")
+  end
+end
