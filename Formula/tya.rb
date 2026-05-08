5632d64511f75461bb724ff168cdb5c885776b17
Masaki Komagata <komagata@gmail.com>
2026-05-08 11:05:52 +0900

    Update tya to v0.4.0

--- Formula/tya.rb
+++ Formula/tya.rb
@@ -1,8 +1,8 @@
 class Tya < Formula
   desc "Small indentation-based dynamic language"
   homepage "https://github.com/komagata/tya"
-  url "https://github.com/komagata/tya/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "1b8f0a0adfa7b03107eda3ed1f9539a5cdb7547d2eab5b57772f3019d32593bd"
+  url "https://github.com/komagata/tya/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "051db12bf9d02807bffb6647ff4bf0d5037a1edce770f16a3d4df0a3a4d13d26"
   head "https://github.com/komagata/tya.git", branch: "main"
 
   depends_on "go" => :build
@@ -10,14 +10,25 @@ class Tya < Formula
   def install
     system "go", "build", *std_go_args(output: bin/"tya"), "./cmd/tya"
     (pkgshare/"runtime").install Dir["runtime/*"]
+    (pkgshare/"stdlib").install Dir["stdlib/*"]
   end
 
   test do
     (testpath/"hello.tya").write <<~TYA
+      import string
+
       print "Hello, Tya"
+      print string.blank(" ")
+    TYA
+    (testpath/"hello_test.tya").write <<~TYA
+      import string
+
+      assert string.blank(" ")
+      assert_equal false, string.blank("tya")
     TYA
 
-    assert_equal "0.2.0\n", shell_output("#{bin}/tya version")
-    assert_equal "Hello, Tya\n", shell_output("#{bin}/tya run #{testpath}/hello.tya")
+    assert_equal "0.4.0\n", shell_output("#{bin}/tya version")
+    assert_equal "Hello, Tya\ntrue\n", shell_output("#{bin}/tya run #{testpath}/hello.tya")
+    assert_empty shell_output("#{bin}/tya test #{testpath}/hello_test.tya")
   end
 end
