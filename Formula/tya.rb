c9d913ecd3e077d8f6a9f1df9beb321aff399317
Masaki Komagata <komagata@gmail.com>
2026-05-10 01:27:24 +0900

    Update tya to v0.37.0

--- Formula/tya.rb
+++ Formula/tya.rb
@@ -1,8 +1,8 @@
 class Tya < Formula
   desc "Small indentation-based dynamic language"
   homepage "https://github.com/komagata/tya"
-  url "https://github.com/komagata/tya/archive/refs/tags/v0.36.0.tar.gz"
-  sha256 "f490612a075474e5e65876b48efabe7c39e6ce9be09480a50f09abbb5306fc92"
+  url "https://github.com/komagata/tya/archive/refs/tags/v0.37.0.tar.gz"
+  sha256 "428cf67ced4505699e48b1e38274bf1a45043e3e74e04d7ba031e57b575c9522"
   head "https://github.com/komagata/tya.git", branch: "main"
 
   depends_on "go" => :build
@@ -27,7 +27,7 @@ class Tya < Formula
       assert_equal false, string.blank("tya")
     TYA
 
-    assert_equal "0.36.0\n", shell_output("#{bin}/tya version")
+    assert_equal "0.37.0\n", shell_output("#{bin}/tya version")
     assert_equal "Hello, Tya\ntrue\n", shell_output("#{bin}/tya run #{testpath}/hello.tya")
     assert_empty shell_output("#{bin}/tya test #{testpath}/hello_test.tya")
   end
