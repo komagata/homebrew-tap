8c43f33598a6d5ad0d9ec61f5d096f29ea3c0b7c
Masaki Komagata <komagata@gmail.com>
2026-05-30 18:12:25 +0900

    tya v0.72.7

--- Formula/tya.rb
+++ Formula/tya.rb
@@ -1,8 +1,8 @@
 class Tya < Formula
   desc "Small indentation-based dynamic language"
   homepage "https://github.com/komagata/tya"
-  url "https://github.com/komagata/tya/archive/refs/tags/v0.72.6.tar.gz"
-  sha256 "d82091deb812923a561b65cc77243bb139ed47f32d7ce7f847e59b5f80eb7080"
+  url "https://github.com/komagata/tya/archive/refs/tags/v0.72.7.tar.gz"
+  sha256 "022880c57785318777041bbf229a772535251c5fca5d5474b2d58bd1cbce2b5f"
   license "MIT"
   head "https://github.com/komagata/tya.git", branch: "main"
 
@@ -32,7 +32,7 @@ class Tya < Formula
       print("  ".blank?())
     TYA
 
-    assert_equal "0.72.6\n", shell_output("#{bin}/tya version")
+    assert_equal "0.72.7\n", shell_output("#{bin}/tya version")
     assert_equal "Hello, Tya\ntrue\n", shell_output("#{bin}/tya run #{testpath}/hello.tya")
 
     # v0.49: `tya new` scaffolds a minimal project tree.
